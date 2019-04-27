ae3708227cb1018a1570cf4f95f4b4a57c6e2917
amishapandey <45044497+amishapandey@users.noreply.github.com>
2019-04-27 12:28:27 +0530

    Create sInnerclass.java
    use of static inner class and static variable

---
+++ sInnerclass.java
@@ -0,0 +1,14 @@
+class Example{
+    int x;
+    static int y;
+    public static class Test{
+        public static String country="Sri Lanka";
+    }
+    }
+    public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println(Example.y=5); 
+       System.out.println(Example.Test.country);
+     }
+}
